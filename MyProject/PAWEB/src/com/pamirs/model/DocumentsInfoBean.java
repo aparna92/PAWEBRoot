d2ce41d5e34af859f4f992eaef24c4f164524b4c
quickbook <subbuvmca@gmail.com>
2015-03-05 23:11:33 -0800

    Rest service business layer

---
+++ MyProject/PAWEB/src/com/pamirs/model/DocumentsInfoBean.java
@@ -0,0 +1,52 @@
+package com.pamirs.model;
+
+import java.util.List;
+ 
+public class DocumentsInfoBean {
+
+	
+	private List documentsInfoList;
+	private  Integer startingIndex;
+	private  Integer itemsCount;
+	
+	
+	
+	
+	
+	
+
+	public List getDocumentsInfoList() {
+		return documentsInfoList;
+	}
+
+	public void setDocumentsInfoList(List documentsInfoList) {
+		this.documentsInfoList = documentsInfoList;
+	}
+
+	public Integer getStartingIndex() {
+		return startingIndex;
+	}
+
+	public void setStartingIndex(Integer startingIndex) {
+		this.startingIndex = startingIndex;
+	}
+
+	public Integer getItemsCount() {
+		return itemsCount;
+	}
+
+	public void setItemsCount(Integer itemsCount) {
+		this.itemsCount = itemsCount;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
